6d61cac3759f6ef12605f93b7723f64a82fbdf3d
zhaofei <13700530350@163.com>
2023-11-01 00:04:31 +0800

    修改

--- chinazImg.py
+++ chinazImg.py
@@ -81,7 +81,7 @@ class Procuder(object):
             img_url = BASE_URL + href
             img_soup = self.get_soup(img_url)
 
-            print(img_soup.find('a', class_='colbule'))
+            print(img_soup.find('div', class_='com-bule-a-box'))
             img_data = {}
             img_data['name'] = img_soup.find('h1').text
             # img_data['desc'] = img_soup.find('a', class_='colbule').get('title')
